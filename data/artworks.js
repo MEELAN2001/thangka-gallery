1bc8f9bac7f35b3fe6394f00c4d09c91ab494974
MEELAN2001 <2001milantamang@gmail.com>
2026-05-02 15:08:00 +0545

    Initial commit: Himalayan Thangka Gallery website with gallery, reviews, FAQ, and pagination

---
+++ data/artworks.js
@@ -0,0 +1,14 @@
+const artworks = [
+  {
+    id: 1,
+    title: "Avalokiteshvara",
+    image: "images/thangka1.jpg",
+    description: "Bodhisattva of compassion"
+  },
+  {
+    id: 2,
+    title: "Mandala",
+    image: "images/thangka2.jpg",
+    description: "Sacred geometric diagram"
+  }
+];
